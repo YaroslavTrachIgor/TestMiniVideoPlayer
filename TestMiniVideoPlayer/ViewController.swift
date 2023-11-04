0f0865a8682cdbbb127d763e932be49bbfbf0c5a
Yaroslav Trach <zhbr282@gmail.com>
2023-11-03 22:02:54 -0400

    Initial Commit

---
+++ TestMiniVideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMiniVideoPlayer
+//
+//  Created by User on 2023-11-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
