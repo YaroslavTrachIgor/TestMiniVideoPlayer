6aecedf3aa9a7b7985611913939d2b2fad9d1e67
Yaroslav Trach <zhbr282@gmail.com>
2023-11-06 19:07:05 -0500

    Updated UI Commit

--- TestMiniVideoPlayer/Modules/VideosList/Cells/VideoTableViewCell.swift
+++ TestMiniVideoPlayer/Modules/VideosList/Cells/VideoTableViewCell.swift
@@ -12,6 +12,22 @@ import AVKit
 final class VideoTableViewCell: UITableViewCell {
     
     //MARK: @IBOutlets
+    @IBOutlet weak var contentBackgroundBlurView: UIVisualEffectView! {
+        didSet {
+            contentBackgroundBlurView.clipsToBounds = true
+            contentBackgroundBlurView.layer.cornerRadius = 20
+            contentBackgroundBlurView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        }
+    }
+    @IBOutlet weak var backgroundContentView: UIView! {
+        didSet {
+            backgroundContentView.layer.cornerRadius = 20
+            backgroundContentView.layer.shadowColor = UIColor.systemGray2.cgColor
+            backgroundContentView.layer.shadowRadius = 8
+            backgroundContentView.layer.shadowOffset = CGSize(width: 0, height: 2)
+            backgroundContentView.layer.shadowOpacity = 0.7
+        }
+    }
     @IBOutlet weak var previewVideoView: PreviewVideoView!
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
@@ -28,6 +44,3 @@ final class VideoTableViewCell: UITableViewCell {
         sender.setImage(previewVideoView.unmute(), for: .normal)
     }
 }
-
-
-
